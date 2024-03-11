935a815416daa9150b7351f94c608082375d01d1
MariioM <mabros2004@gmail.com>
2024-03-11 12:39:57 +0100

    Agregar archivos de proyecto.

---
+++ XBet/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace XBet
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ XBet/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace XBet
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
